c13cf1e624a9fb91451a294b41ede27c7c61d3b2
MikeNkunku <mikenkunku@gmail.com>
2017-09-06 06:00:15 +0200

    [ADD] playlist_writing.get_playlist_file_path : implementations + UT.

--- app/playlist_writing.py
+++ app/playlist_writing.py
@@ -1,9 +1,22 @@
+from string import join, rsplit
+
+
+# Global variables
+PLAYLIST_EXTENSION = '.xspf'
+PLAYLIST_NAME_SUFFIX = '(follow-up)'
+
+
 def get_playlist_file_path(i_folder_absolute_path):
     '''
     Returns the playlist file name.
     i_folder_absolute_path : The absolute path of the folder in which the elements of the playlist are contained.
     '''
-    pass
+    folders = i_folder_absolute_path.split('\\')
+    playlist_file_name_components = folders[-2:]
+    playlist_file_name_components.append(PLAYLIST_NAME_SUFFIX)
+    playlist_file_name = join(playlist_file_name_components, " ") + PLAYLIST_EXTENSION
+    return rsplit(i_folder_absolute_path, '\\', 1)[0] + '\\' + playlist_file_name
+
 
 def get_file_absolute_path_from_playlist_element(i_element_absolute_file_path):
     '''
@@ -12,12 +25,14 @@ def get_file_absolute_path_from_playlist_element(i_element_absolute_file_path):
     '''
     pass
 
+
 def write_playlist_start(i_playlist_file_path):
     '''
     Writes the beginning of the XSPF file.
     i_playlist_file_path : The absolute path of the XSPF playlist file. 
     '''
     pass
+
 
 def write_playlist_element(i_playlist_file_path, i_idx, i_element_absolute_path):
     '''
@@ -27,6 +42,7 @@ def write_playlist_element(i_playlist_file_path, i_idx, i_element_absolute_path)
     i_element_absolute_path : The absolute path of the element.
     '''
     pass
+
 
 def write_playlist_end(i_playlist_file_path):
     '''
--- app/tests/PlaylistWritingTest.py
+++ app/tests/PlaylistWritingTest.py
@@ -1,11 +1,13 @@
 import unittest
+from playlist_writing import get_playlist_file_path, PLAYLIST_EXTENSION, PLAYLIST_NAME_SUFFIX
 
 
 class PlaylistWritingTest(unittest.TestCase):
 
-    def testName(self):
-        pass
-
+    def test_get_playlist_file_path(self):
+        playlist_file_path = get_playlist_file_path('C:\\Daredevil\\S02')
+        expected = 'C:\\Daredevil\\Daredevil S02 ' + PLAYLIST_NAME_SUFFIX + PLAYLIST_EXTENSION
+        self.assertEqual(expected, playlist_file_path, 'Expected "{0}" to be equal to "{1}".'.format(playlist_file_path, expected))
 
 if __name__ == "__main__":
     unittest.main()
