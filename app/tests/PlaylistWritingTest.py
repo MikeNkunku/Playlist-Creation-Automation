e10e35edd13059acb8263435e664dd6e61ab5aba
MikeNkunku <mikenkunku@gmail.com>
2017-09-10 00:12:13 +0200

    [ADD] assertFileContains & test_write_playlist_start
    - Added custom method to check file contents ;
    - Added UT for playlist_writing.write_playlist_start.

--- app/tests/PlaylistWritingTest.py
+++ app/tests/PlaylistWritingTest.py
@@ -1,10 +1,27 @@
 import unittest
+from os import remove
+from tempfile import gettempdir
 from playlist_writing import *
 
 
 class PlaylistWritingTest(unittest.TestCase):
 
     TV_SERIES_FOLDER = 'C:\\Daredevil\\S02'
+
+    def assertFileContains(self, i_file_to_read_descriptor, i_string_to_look):
+        ''''
+            Returns whether the element was found.
+            i_file_to_read_descriptor : File descriptor for the element to read
+            i_string_to_look          : The String to look for
+        '''
+
+        found = False
+        with open(i_file_to_read_descriptor.name, mode='r') as fp:
+            for line in fp.readlines():
+                if line.find(i_string_to_look) != -1:
+                    found = True
+            fp.close()
+            return found
 
 
     def test_get_playlist_file_path(self):
@@ -17,6 +34,17 @@ class PlaylistWritingTest(unittest.TestCase):
         file_protocol_element_path = get_file_absolute_path_from_playlist_element(self.TV_SERIES_FOLDER + '\\' + 'Daredevil S02E01.mkv')
         expected = 'file:///C:/Daredevil/S02/Daredevil%20S02E01.mkv'
         self.assertEqual(expected, file_protocol_element_path, 'Expected "{0}" to be equal to "{1}".'.format(file_protocol_element_path, expected))
-    
+
+
+    def test_write_playlist_start(self):
+        filePath = get_playlist_file_path(gettempdir())
+        with open(filePath, mode='w+b') as f:
+            write_playlist_start(f)
+            for expected_elt in ['<?xml', '<playlist xmlns="', '<title>', '<trackList>']:
+                self.assertTrue(self.assertFileContains(f, expected_elt))
+
+        remove(filePath)
+
+
 if __name__ == "__main__":
     unittest.main()
