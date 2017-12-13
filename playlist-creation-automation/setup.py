24d48ef965ff7ef2e1df3abd748d38ed8705a075
MikeNkunku <mikenkunku@gmail.com>
2017-12-13 16:11:17 +0100

    Added setup.py.

---
+++ playlist-creation-automation/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Playlist Creation Automation',
+    version='1.0',
+    descripttion='This project aims at easing the creation of VLC playlists from Windows command line.',
+    author='Mike Nkunku',
+    packages=['bin']
+)
