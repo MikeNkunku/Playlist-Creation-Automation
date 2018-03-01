827767ed056c6a4651ff259778d5c2d214eae3ad
MikeNkunku <mikenkunku@gmail.com>
2018-03-01 23:30:02 +0100

    Fixed method documentation according to PEP8.

--- playlist-creation-automation/tests/PlaylistWritingTest.py
+++ playlist-creation-automation/tests/PlaylistWritingTest.py
@@ -8,7 +8,8 @@ class PlaylistWritingTest(unittest.TestCase):
 
     TV_SERIES_FOLDER = 'C:\\Daredevil\\S02'
 
-    def assertFileContains(self, i_file_to_read_descriptor, i_string_to_look):
+    @staticmethod
+    def assertFileContains(i_file_to_read_descriptor, i_string_to_look):
         """
             Returns whether the element was found.
 
@@ -24,27 +25,24 @@ class PlaylistWritingTest(unittest.TestCase):
             fp.close()
             return found
 
-
     def test_get_playlist_file_path(self):
         playlist_file_path = get_playlist_file_path(self.TV_SERIES_FOLDER)
         expected = 'C:\\Daredevil\\Daredevil S02' + PLAYLIST_NAME_SUFFIX + PLAYLIST_EXTENSION
         self.assertEqual(expected, playlist_file_path, 'Expected "{0}" to be equal to "{1}".'.format(playlist_file_path, expected))
-
 
     def test_get_file_absolute_path_from_playlist_element(self):
         file_protocol_element_path = get_file_absolute_path_from_playlist_element(self.TV_SERIES_FOLDER + '\\' + 'Daredevil S02E01.mkv')
         expected = 'file:///C:/Daredevil/S02/Daredevil%20S02E01.mkv'
         self.assertEqual(expected, file_protocol_element_path, 'Expected "{0}" to be equal to "{1}".'.format(file_protocol_element_path, expected))
 
-
     def test_write_playlist_start(self):
-        filePath = get_playlist_file_path(gettempdir())
-        with open(filePath, mode='w+b') as f:
+        file_path = get_playlist_file_path(gettempdir())
+        with open(file_path, mode='w+b') as f:
             write_playlist_start(f)
             for expected_elt in ['<?xml', '<playlist xmlns="', '<title>', '<trackList>']:
                 self.assertTrue(self.assertFileContains(f, expected_elt))
 
-        remove(filePath)
+        remove(file_path)
 
 
 if __name__ == "__main__":
