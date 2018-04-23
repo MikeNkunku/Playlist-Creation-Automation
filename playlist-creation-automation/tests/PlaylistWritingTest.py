b497eb8d4e6381ecf67b6355a132662ada1809a6
MikeNkunku <mikenkunku@gmail.com>
2018-04-23 23:05:45 +0200

    Fixed arguments order in assertions.
    Also added missing early exit in loop.

--- playlist-creation-automation/tests/PlaylistWritingTest.py
+++ playlist-creation-automation/tests/PlaylistWritingTest.py
@@ -6,7 +6,7 @@ from playlist_writing import *
 
 class PlaylistWritingTest(unittest.TestCase):
 
-    TV_SERIES_FOLDER = 'C:\\Daredevil\\S02'
+    TV_SERIES_FOLDER = "C:\\Daredevil\\S02"
 
     @staticmethod
     def assertFileContains(i_file_to_read_descriptor, i_string_to_look: str) -> bool:
@@ -23,26 +23,36 @@ class PlaylistWritingTest(unittest.TestCase):
             for line in fp.readlines():
                 if line.find(i_string_to_look) != -1:
                     found = True
+                    break
             fp.close()
             return found
 
     def test_get_playlist_file_path(self):
         playlist_file_path = get_playlist_file_path(self.TV_SERIES_FOLDER)
-        expected = 'C:\\Daredevil\\Daredevil S02' + PLAYLIST_NAME_SUFFIX + PLAYLIST_EXTENSION
-        self.assertEqual(expected, playlist_file_path, 'Expected "{0}" to be equal to "{1}".'.format(playlist_file_path, expected))
+        expected = "C:\\Daredevil\\Daredevil S02" + PLAYLIST_NAME_SUFFIX + PLAYLIST_EXTENSION
+        self.assertEqual(
+            playlist_file_path, expected,
+            "Expected '{0}' to be equal to '{1}'.".format(playlist_file_path, expected)
+        )
 
     def test_get_file_absolute_path_from_playlist_element(self):
-        abs_path = self.TV_SERIES_FOLDER + '\\' + 'Daredevil S02E01.mkv'
+        abs_path = self.TV_SERIES_FOLDER + '\\' + "Daredevil S02E01.mkv"
         file_protocol_element_path = get_file_absolute_path_from_playlist_element(abs_path)
-        expected = 'file:///C:/Daredevil/S02/Daredevil%20S02E01.mkv'
-        self.assertEqual(expected, file_protocol_element_path, 'Expected "{0}" to be equal to "{1}".'.format(file_protocol_element_path, expected))
+        expected = "file:///C:/Daredevil/S02/Daredevil%20S02E01.mkv"
+        self.assertEqual(
+            file_protocol_element_path, expected,
+            "Expected '{0}' to be equal to '{1}'.".format(file_protocol_element_path, expected)
+        )
 
     def test_write_playlist_start(self):
         file_path = get_playlist_file_path(os.path.join(tempdir.TempDir().name, 'test' + PLAYLIST_EXTENSION))
         with open(file_path, mode='w') as f:
             write_playlist_start(f)
             for expected_elt in ['<?xml', '<playlist xmlns="', '<title>', '<trackList>']:
-                self.assertTrue(self.assertFileContains(f, expected_elt))
+                self.assertTrue(
+                    self.assertFileContains(f, expected_elt),
+                    "'{0}' does not contain '{1}'".format(file_path, expected_elt)
+                )
 
         tempdir.remove(file_path)
 
